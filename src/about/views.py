f2d1efa827d46b0d8726f9a0b040e4b9e7ec390f
saposki <saposki@gmail.com>
2016-06-13 08:39:06 -0700

    modified "about/views.py" to query data base

--- src/about/views.py
+++ src/about/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+from . models import About
 
 def about(request):
+    queryset = About.object.all()
     context = {
-        'title': 'description'
+        'title': 'about'
     }
     return render(request,'about.html', context)
