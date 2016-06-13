9d3a6fcb4e1063b68817921565c6ffdcb74ffdc4
saposki <saposki@gmail.com>
2016-06-13 10:16:52 -0700

    modified "about/models.py" modified text field maximum length

--- src/about/models.py
+++ src/about/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # Create your models here.
 class About(models.Model):
     title = models.CharField(max_length=120)
-    description = models.TextField(max_length=120)
+    description = models.TextField(max_length=500)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
 
