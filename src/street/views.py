088a986e532b2a543fa9792e0ca28f380e7436fd
saposki <saposki@gmail.com>
2016-06-13 16:35:07 -0700

    registered "street app"

--- src/street/views.py
+++ src/street/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from . models import Street
+
+def street(request):
+    queryset = Street.objects.all()
+    context = {
+        'image' : queryset
+    }
+    return render(request, 'street.html', context)
