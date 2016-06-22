3996c34d5e421e0e71484ab295a97ff77cf7ecf3
saposki <saposki@gmail.com>
2016-06-22 02:23:01 -0700

    static files setup

--- src/saposki/urls.py
+++ src/saposki/urls.py
@@ -13,7 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
@@ -24,3 +27,6 @@ urlpatterns = [
     url(r'^crasher/', include('crasher.urls', namespace='crasher')),
     url(r'^street/', include('street.urls', namespace='street')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
